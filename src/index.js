d54a78c1abb7a9a782f4f04654c61cb42a88e2f4
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-10-05 15:32:08 +0200

    Create skeleton for the leaderboard content. - generate table scores using javascript. - create form for adding scores.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash is required for this line to work
-    element.innerHTML = _.join(['Hello', 'Leaderboard'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+const scoreData = [
+  {name: "name", score: 100,},
+  {name: "name", score: 100,},
+  {name: "name", score: 100,},
+  {name: "name", score: 100,},
+  {name: "name", score: 100,},
+  {name: "name", score: 100,},
+]
+
+function loadTable() {
+  const table = document.querySelector('.scoreboard-table');
+
+  scoreData.forEach((data) => {
+    const tr = document.createElement('tr');
+    tr.className = 'table-row';
+    const td = document.createElement('td');
+    td.className = 'table-data';
+    td.innerHTML = `${data.name}: ${data.score}`;
+
+    tr.appendChild(td);
+    table.appendChild(tr);
+  });
+}
+
+loadTable();
